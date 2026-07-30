0fba59d0ac0ffec9552a9dc882d50c3a197ae777
chaijewon <vcandjava@nate.com>
2026-07-30 09:27:52 +0900

    first

---
+++ src/main/java/com/sist/web/MainController.java
@@ -0,0 +1,80 @@
+package com.sist.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import com.sist.service.*;
+import com.sist.vo.*;
+
+import lombok.RequiredArgsConstructor;
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+   private final FoodService fService;
+	
+   @GetMapping("main/main.do")
+   public String main_main(String page,Model model,HttpServletRequest request)
+   {
+	   if(page==null)
+		   page="1";
+	   int curpage=Integer.parseInt(page);
+	   final int ROWSIZE=12;
+	   int start=(ROWSIZE*curpage)-(ROWSIZE-1);
+	   int end=ROWSIZE*curpage;
+	   // OFFSET  /  rownum 
+	   //  | 0       | 1
+	   List<FoodVO> list=fService.foodListData(start, end);
+	   int totalpage=fService.foodTotalPage();
+	   
+	   final int BLOCK=10;
+	   int startPage=((curpage-1)/BLOCK*BLOCK)+1;
+	   //              ------- 10
+	   int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
+	   
+	   if(endPage>totalpage)
+		   endPage=totalpage;
+	   
+	   model.addAttribute("list", list);
+	   model.addAttribute("curpage", curpage);
+	   model.addAttribute("totalpage", totalpage);
+	   model.addAttribute("startPage", startPage);
+	   model.addAttribute("endPage", endPage);
+	   
+	   model.addAttribute("main_jsp", "../main/home.jsp");
+	   
+	   List<FoodVO> cList=new ArrayList<FoodVO>();
+	   Cookie[] cookies=request.getCookies();
+	   if(cookies!=null)
+	   {
+		   // 최신순 
+		   for(int i=cookies.length-1;i>=0;i--)
+		   {
+			   if(cookies[i].getName().startsWith("food_"))
+			   {
+				   if(cookies[i].getName().equals("food_null"))
+					   continue;
+				   
+				   String no=cookies[i].getValue();
+				   
+				   FoodVO vo=fService.foodDetailData(Integer.parseInt(no));
+				   
+				   cList.add(vo);
+			   }
+		   }
+	   }
+	   model.addAttribute("cList", cList);
+	   model.addAttribute("size", cList.size());
+	   /*
+	    *   내장객체의 사용처 
+	    *     request/response => cookie/ fileupload
+	    *     session => 보안 / 회원 관련 
+	    *     RedirectAttributes : 이미 있는 화면으로 이동 
+	    */
+	   return "main/main";
+   }
+}
